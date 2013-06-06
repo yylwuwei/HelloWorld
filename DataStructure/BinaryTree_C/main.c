551dbba1479c602bb9ddc77082ba337081c387d0
yeyulin <yeyulin1216@163.com>
2013-06-06 10:33:11 +0800

    FixProj: BinaryTree框架确定。

--- DataStructure/BinaryTree_C/main.c
+++ DataStructure/BinaryTree_C/main.c
@@ -1,5 +1,50 @@
+#include <stdio.h>
+
+#define TRUE 1
+#define FALSE -1
+
+typedef int ElemType; 
+typedef int BOOL;
+
+typedef struct _BinaryTreeNode
+{
+	ElemType elem;
+	struct _BinaryTreeNode* left;
+	struct _BinaryTreeNode* right;
+}BinareTreeNode;
+
+void InitBinaryTree()
+{
+
+}
+
+void ShowBinaryTree()
+{
+
+}
+
+void InsertBinaryTreeNode()
+{
+
+}
+
+BOOL SearchBinaryTreeNode()
+{
+	return FALSE;
+}
+
+void DeleteBinaryTreeNode()
+{
+
+}
+
 int main()
 {
+	InitBinaryTree();
+	InsertBinaryTreeNode();
+	SearchBinaryTreeNode();
+	DeleteBinaryTreeNode();
+	ShowBinaryTree();
 	printf("binary tree journey!\n");
 	return 0;
 }

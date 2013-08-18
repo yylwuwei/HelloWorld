ecfb036efe92269f38e30de271e8fb540a6d1345
yeyulin <yeyulin1216@163.com>
2013-08-18 17:23:14 +0800

    AddFile: MFCDlgStudy 项目首次提交。

---
+++ QuestionStudy/MFCDlgStudy/MFCDlgStudy.h
@@ -0,0 +1,32 @@
+
+// MFCDlgStudy.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMFCDlgStudyApp:
+// See MFCDlgStudy.cpp for the implementation of this class
+//
+
+class CMFCDlgStudyApp : public CWinAppEx
+{
+public:
+	CMFCDlgStudyApp();
+
+// Overrides
+	public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCDlgStudyApp theApp;
---
+++ QuestionStudy/MFCDlgStudy/MFCDlgStudyDlg.h
@@ -0,0 +1,32 @@
+
+// MFCDlgStudyDlg.h : header file
+//
+
+#pragma once
+
+
+// CMFCDlgStudyDlg dialog
+class CMFCDlgStudyDlg : public CDialog
+{
+// Construction
+public:
+	CMFCDlgStudyDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_MFCDLGSTUDY_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
